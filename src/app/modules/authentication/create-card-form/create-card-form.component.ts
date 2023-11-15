589b5544ea11584a7221162ded820db22f8ddd76
Christian <arthurchristian111@gmail.com>
2023-11-15 21:40:06 +0000

    .

--- src/app/modules/authentication/create-card-form/create-card-form.component.ts
+++ src/app/modules/authentication/create-card-form/create-card-form.component.ts
@@ -3,6 +3,15 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsersService } from 'src/app/core/services/user/users.service';
+import { CollectionReference } from '@angular/fire/firestore';
+import {
+  ref,
+  uploadBytesResumable,
+  Storage,
+  getDownloadURL,
+} from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-create-card-form',
@@ -15,12 +24,18 @@ export class CreateCardFormComponent {
   isFetching: boolean = false;
   image: any;
   cardName: any;
+  private path = 'Users';
+  users$: Observable<any[]> | undefined;
+  usersCollection: CollectionReference | any;
+  imageUrl: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private route: Router,
     private usersService: UsersService,
-    private authService: AuthService
+    private authService: AuthService,
+    private storage: Storage,
+    private db: AngularFirestore
   ) {}
 
   buildCardForm(): void {
@@ -56,6 +71,30 @@ export class CreateCardFormComponent {
   changeCard(e: any) {
     this.cardName?.setValue(e.target.value, {});
   }
+
+  // uploading fire to firebase
+  // public uploadFile(input: HTMLInputElement) {
+  //   if (!input.files) return;
+
+  //   const files: FileList = input.files;
+  //   console.error(files);
+
+  //   if (files.length) {
+  //     const storageRef = ref(this.storage, files[0].name);
+  //     uploadBytesResumable(storageRef, files[0]).then(async () => {
+  //       this.imageUrl = await getDownloadURL(storageRef);
+  //     });
+  //   }
+  // }
+
+  // private getImages(): void {
+  //   this.db
+  //     .collection(this.path)
+  //     .snapshotChanges()
+  //     .subscribe((res: any) => {
+  //       console.log('User', res);
+  //     });
+  // }
 
   onSubmit() {
     this.submitted = true;
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  // firebaseConfig: {
+  //   apiKey: 'AIzaSyDxV9hEBX1e4kRrdyqBomyL0TT0wYTwpP4',
+  //   authDomain: 'doctor-s-appointments.firebaseapp.com',
+  //   databaseURL: 'https://doctor-s-appointments-default-rtdb.firebaseio.com',
+  //   projectId: 'doctor-s-appointments',
+  //   storageBucket: 'doctor-s-appointments.appspot.com',
+  //   messagingSenderId: '672588399008',
+  //   appId: '1:672588399008:web:2ee5a3750d46bd5b4c64c5',
+  // },
+
+  production: false,
+};
