e3a74f63d6647d8df5d4d8ed4aae3b6f536e9158
Christian <arthurchristian111@gmail.com>
2023-11-19 23:23:46 +0000

    toggle menu

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  show: boolean = false;
+
   toggleMenu() {
+    this.show = !this.show;
     document.getElementById('navbarSupportedContent')?.classList.toggle('show');
   }
 
