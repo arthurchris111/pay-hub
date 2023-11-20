3715f901aaa91331e5ebe7e98dc316c8e8bd4d07
Christian <arthurchristian111@gmail.com>
2023-11-20 22:02:19 +0000

    changes

--- src/app/modules/authentication/create-card-form/create-card-form.component.ts
+++ src/app/modules/authentication/create-card-form/create-card-form.component.ts
@@ -55,7 +55,7 @@ export class CreateCardFormComponent {
           Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
         ],
       ],
-      password: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
       date: ['', [Validators.required]],
       idNumber: ['', [Validators.required]],
       cardType: ['', [Validators.required]],
