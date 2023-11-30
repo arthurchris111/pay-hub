8c36e432ff1af4dcf2d38661951eea279f5d83cc
Christian <arthurchristian111@gmail.com>
2023-11-30 22:10:24 +0000

    add api key

--- src/app/core/interceptor/http_interceptor.ts
+++ src/app/core/interceptor/http_interceptor.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class httpInterceptor implements HttpInterceptor {
-  // private apKey = 'sk.179555b.3d453a1040679cedadfe4c6ef';
+  private apKey = 'sk.179555b.3d453a1040679cedadfe4c6ef';
   private apiKey = 'sk.8db7e70.2bb8ca57be096f08656265bde';
   private apiUrl = 'https://staging-api.flowertop.xyz/api/v1/virtualcards/create';
 
